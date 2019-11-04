4c894bc0fe8fc6c38f1568e8381cb8d04b77250b
Mahesh Amruthaluri <mahesh.amruthaluri@ibm.com>
2019-11-04 08:36:06 -0600

    Removed app.py and request_handler.py

--- request_handler.py
+++
@@ -1,5 +0,0 @@
-from flask_restplus import reqparse
-
-TARGET_SENTENCE = reqparse.RequestParser(bundle_errors=True)
-TARGET_SENTENCE.add_argument('word', type=str, required=False, location='args',
-                             help='count the number of occurrences')
